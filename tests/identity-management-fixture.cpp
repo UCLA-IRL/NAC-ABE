f7a9579831a789de2daa6ccf6cf94131d6669706
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2017-06-04 23:09:27 -0700

    Fix unit test to pass compilation
    Change-Id: Ie9ebafcfe4ce2ca784a082b692f28b970a1cd324

--- tests/identity-management-fixture.cpp
+++ tests/identity-management-fixture.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2014-2016, Regents of the University of California,
+ * Copyright (c) 2014-2017, Regents of the University of California,
  *                          Arizona Board of Regents,
  *                          Colorado State University,
  *                          University Pierre & Marie Curie, Sorbonne University,
@@ -9,20 +9,20 @@
  *                          The University of Memphis.
  *
  * This file, originally written as part of NFD (Named Data Networking Forwarding Daemon),
- * is a part of ChronoShare, a decentralized file sharing application over NDN.
+ * is a part of ndncert, a certificate management system based on NDN.
  *
- * ChronoShare is free software: you can redistribute it and/or modify it under the terms
+ * ndncert is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
- * ChronoShare is distributed in the hope that it will be useful, but WITHOUT ANY
+ * ndncert is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received copies of the GNU General Public License along with
- * ChronoShare, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ * ndncert, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  *
- * See AUTHORS.md for complete list of ChronoShare authors and contributors.
+ * See AUTHORS.md for complete list of ndncert authors and contributors.
  */
 
 #include "identity-management-fixture.hpp"
@@ -35,16 +35,8 @@ namespace ndnabac {
 namespace tests {
 
 IdentityManagementFixture::IdentityManagementFixture()
-  : m_keyChain("pib-memory", "tmp-memory:")
+  : m_keyChain("pib-memory:", "tpm-memory:")
 {
-}
-
-IdentityManagementFixture::~IdentityManagementFixture()
-{
-  boost::system::error_code ec;
-  for (const auto& certFile : m_certFiles) {
-    boost::filesystem::remove(certFile, ec); // ignore error
-  }
 }
 
 security::Identity
@@ -56,6 +48,69 @@ IdentityManagementFixture::addIdentity(const Name& identityName, const KeyParams
 }
 
 bool
+IdentityManagementFixture::saveIdentityCertificate(const security::Identity& identity,
+                                                     const std::string& filename)
+{
+  try {
+    auto cert = identity.getDefaultKey().getDefaultCertificate();
+    return saveCertToFile(cert, filename);
+  }
+  catch (const security::Pib::Error&) {
+    return false;
+  }
+}
+
+security::Identity
+IdentityManagementFixture::addSubCertificate(const Name& subIdentityName,
+                                               const security::Identity& issuer, const KeyParams& params)
+{
+  auto subIdentity = addIdentity(subIdentityName, params);
+
+  security::v2::Certificate request = subIdentity.getDefaultKey().getDefaultCertificate();
+
+  request.setName(request.getKeyName().append("parent").appendVersion());
+
+  SignatureInfo info;
+  info.setValidityPeriod(security::ValidityPeriod(time::system_clock::now(),
+                                                  time::system_clock::now() + time::days(7300)));
+
+  security::v2::AdditionalDescription description;
+  description.set("type", "sub-certificate");
+  info.appendTypeSpecificTlv(description.wireEncode());
+
+  m_keyChain.sign(request, signingByIdentity(issuer).setSignatureInfo(info));
+  m_keyChain.setDefaultCertificate(subIdentity.getDefaultKey(), request);
+
+  return subIdentity;
+}
+
+security::v2::Certificate
+IdentityManagementFixture::addCertificate(const security::Key& key, const std::string& issuer)
+{
+  Name certificateName = key.getName();
+  certificateName
+    .append(issuer)
+    .appendVersion();
+  security::v2::Certificate certificate;
+  certificate.setName(certificateName);
+
+  // set metainfo
+  certificate.setContentType(tlv::ContentType_Key);
+  certificate.setFreshnessPeriod(time::hours(1));
+
+  // set content
+  certificate.setContent(key.getPublicKey().buf(), key.getPublicKey().size());
+
+  // set signature-info
+  SignatureInfo info;
+  info.setValidityPeriod(security::ValidityPeriod(time::system_clock::now(),
+                                                  time::system_clock::now() + time::days(10)));
+
+  m_keyChain.sign(certificate, signingByKey(key).setSignatureInfo(info));
+  return certificate;
+}
+
+bool
 IdentityManagementFixture::saveCertToFile(const Data& obj, const std::string& filename)
 {
   m_certFiles.insert(filename);
@@ -64,19 +119,6 @@ IdentityManagementFixture::saveCertToFile(const Data& obj, const std::string& fi
     return true;
   }
   catch (const io::Error&) {
-    return false;
-  }
-}
-
-bool
-IdentityManagementFixture::saveIdentityCertificate(const security::Identity& identity,
-                                                   const std::string& filename)
-{
-  try {
-    auto cert = identity.getDefaultKey().getDefaultCertificate();
-    return saveCertToFile(cert, filename);
-  }
-  catch (const security::Pib::Error&) {
     return false;
   }
 }
--- tests/identity-management-fixture.hpp
+++ tests/identity-management-fixture.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2014-2016, Regents of the University of California,
+ * Copyright (c) 2014-2017, Regents of the University of California,
  *                          Arizona Board of Regents,
  *                          Colorado State University,
  *                          University Pierre & Marie Curie, Sorbonne University,
@@ -9,43 +9,44 @@
  *                          The University of Memphis.
  *
  * This file, originally written as part of NFD (Named Data Networking Forwarding Daemon),
- * is a part of ChronoShare, a decentralized file sharing application over NDN.
+ * is a part of ndncert, a certificate management system based on NDN.
  *
- * ChronoShare is free software: you can redistribute it and/or modify it under the terms
+ * ndncert is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
- * ChronoShare is distributed in the hope that it will be useful, but WITHOUT ANY
+ * ndncert is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received copies of the GNU General Public License along with
- * ChronoShare, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ * ndncert, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  *
- * See AUTHORS.md for complete list of ChronoShare authors and contributors.
+ * See AUTHORS.md for complete list of ndncert authors and contributors.
  */
 
-#ifndef NDNABAC_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
-#define NDNABAC_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
+#ifndef NDNCERT_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
+#define NDNCERT_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
 
 #include "test-common.hpp"
-
-#include <ndn-cxx/security/key-chain.hpp>
+#include <ndn-cxx/security/v2/key-chain.hpp>
+#include <ndn-cxx/security/v2/additional-description.hpp>
+#include <ndn-cxx/security/signing-helpers.hpp>
 
 namespace ndn {
 namespace ndnabac {
 namespace tests {
 
-/** \brief a fixture that cleans up KeyChain identities and certificate files upon destruction
+/**
+ * @brief A test suite level fixture to help with identity management
+ *
+ * Test cases in the suite can use this fixture to create identities.  Identities,
+ * certificates, and saved certificates are automatically removed during test teardown.
  */
-class IdentityManagementFixture : public virtual BaseFixture
+class IdentityManagementFixture
 {
 public:
   IdentityManagementFixture();
-
-  /** \brief deletes created identities and saved certificate files
-   */
-  ~IdentityManagementFixture();
 
   /**
    * @brief Add identity @p identityName
@@ -53,9 +54,6 @@ public:
    */
   security::Identity
   addIdentity(const Name& identityName, const KeyParams& params = security::v2::KeyChain::getDefaultKeyParams());
-
-  bool
-  saveCertToFile(const Data& obj, const std::string& filename);
 
   /**
    *  @brief Save identity certificate to a file
@@ -66,12 +64,32 @@ public:
   bool
   saveIdentityCertificate(const security::Identity& identity, const std::string& filename);
 
-protected:
-  security::v2::KeyChain m_keyChain;
+  /**
+   * @brief Issue a certificate for \p subIdentityName signed by \p issuer
+   *
+   *  If identity does not exist, it is created.
+   *  A new key is generated as the default key for identity.
+   *  A default certificate for the key is signed by the issuer using its default certificate.
+   *
+   *  @return the sub identity
+   */
+  security::Identity
+  addSubCertificate(const Name& subIdentityName, const security::Identity& issuer,
+                    const KeyParams& params = security::v2::KeyChain::getDefaultKeyParams());
 
-private:
+  /**
+   * @brief Add a self-signed certificate to @p key with issuer ID @p issuer
+   */
+  security::v2::Certificate
+  addCertificate(const security::Key& key, const std::string& issuer);
+
+  bool
+  saveCertToFile(const Data& obj, const std::string& filename);
+
+protected:
   std::set<Name> m_identities;
   std::set<std::string> m_certFiles;
+  security::v2::KeyChain m_keyChain;
 };
 
 /** \brief convenience base class for inheriting from both UnitTestTimeFixture
@@ -86,4 +104,4 @@ class IdentityManagementTimeFixture : public UnitTestTimeFixture
 } // namespace ndnabac
 } // namespace ndn
 
-#endif // NDNABAC_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
+#endif // NDNCERT_TESTS_IDENTITY_MANAGEMENT_FIXTURE_HPP
