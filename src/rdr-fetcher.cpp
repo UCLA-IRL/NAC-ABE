32c6213381ef0e377bac465fca6f269897dfaf4c
Sicong Chang <changsiconguw@gmail.com>
2023-03-14 01:44:30 +0000

    change string modification to Name modification

--- src/rdr-fetcher.cpp
+++ src/rdr-fetcher.cpp
@@ -41,12 +41,15 @@ RdrFetcher::onMetaData(const Data& fetchedMetaData)
 
   
   // code to fetch metadata name and get segment names
-  std::string segmentName = ndn::encoding::readString(fetchedMetaData.getName().get(0));
-  // discard the segment number
-  int pos = segmentName.find_last_of("\\");
-  if (pos != std::string::npos) {
-    segmentName = segmentName.substr(0, pos);
-  }
+  
+  // std::string segmentName = ndn::encoding::readString(fetchedMetaData.getName().get(0));
+  // // discard the segment number
+  // int pos = segmentName.find_last_of("\\");
+  // if (pos != std::string::npos) {
+  //   segmentName = segmentName.substr(0, pos);
+  // }
+  
+  PartialName segmentName = fetchedMetaData.getName().getSubName(0, fetchedMetaData.getName().size() - 1);
   NDN_LOG_INFO("segment name is : " << segmentName);
   
   // code to fetch metadata content to know how many segments
@@ -77,21 +80,20 @@ RdrFetcher::onSegmentData(const Data& fetchedSegmentData)
   NDN_LOG_INFO("[onSegmentData()] Get Segment data");
 
   // code to fetch segname to find out sequence number
-  std::string segmentNumber = ndn::encoding::readString(fetchedSegmentData.getName().get(0));
-  size_t pos = segmentNumber.find_last_of("\\");
-  if (pos != string::npos) {
-    segmentNumber = segmentNumber.substr(pos + 1);
-  }
-  NDN_LOG_INFO("Current segment number:  " << segmentNumber);
+  // std::string segmentNumber = ndn::encoding::readString(fetchedSegmentData.getName().get(0));
+  // size_t pos = segmentNumber.find_last_of("\\");
+  // if (pos != string::npos) {
+  //   segmentNumber = segmentNumber.substr(pos + 1);
+  // }
+  PartialName segmentNumberName = fetchedSegmentData.getName().getSubName(-1, 1);
+  NDN_LOG_INFO("Current segment number:  " << segmentNumberName);
   
   // code to fetch segdata content
   Block segContent = fetchedSegmentData.getContent();
   
   // put content buffer into index i = segmentNumber
-  size_t n;
-  stringstream ss(segmentNumber);
-  ss >> n;
-  m_segmentBuffers[n] = Buffer(segContent.value(), segContent.size());
+  uint64_t segmentNumber = segmentNumberName.get(0).toSegment();
+  m_segmentBuffers[segmentNumber] = Buffer(segContent.value(), segContent.size());
 }
 
 } // namespace nacabe
